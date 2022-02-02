e928dad00120856ccbb7f6b4102ce97b3be815e0
wimora123 <wimorasarwindo@yahoo.com>
2022-02-02 22:01:07 +0700

    Update tambahan column photo

--- database/vue1.sql
+++ database/vue1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 02, 2022 at 07:11 AM
+-- Generation Time: Feb 02, 2022 at 03:59 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
@@ -55,7 +55,8 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (4, '2014_10_12_000000_create_users_table', 1),
 (5, '2014_10_12_100000_create_password_resets_table', 1),
-(6, '2019_08_19_000000_create_failed_jobs_table', 1);
+(6, '2019_08_19_000000_create_failed_jobs_table', 1),
+(7, '2022_02_02_135235_add_column_photo_to_user_table', 2);
 
 -- --------------------------------------------------------
 
@@ -81,6 +82,7 @@ CREATE TABLE `users` (
   `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `photo` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -90,14 +92,14 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Amy Weber', 'hpowlowski@feeney.info', NULL, '$2y$10$9L.Vx2qVpgz4RgPG3lKxruCZFfNhmbARXH5avFsaeUeKIJbZOMyKW', NULL, NULL, NULL),
-(2, 'Otha Cartwright', 'xwindler@hotmail.com', NULL, '$2y$10$ts6N9vIdv7eej5NRCXv1eeUaqPLnIjaxC81NOlrJ5B4tmnOrQ8zNO', NULL, NULL, NULL),
-(3, 'Lowell Durgan', 'npowlowski@stanton.org', NULL, '$2y$10$8DQiHob8mKh/aIEq/JOOp.c5FLXQfNrlKmhcKGxTM3BCKNrLEa6GK', NULL, NULL, NULL),
-(4, 'Adrian Grant', 'ssanford@yahoo.com', NULL, '$2y$10$e41GYck2VwvcmYe6srEsU.K9pKoipJX2h7BJvElde4WMXKJT1zcp.', NULL, NULL, NULL),
-(5, 'Linda Gutkowski', 'mertz.tamara@hotmail.com', NULL, '$2y$10$uTMZ8HvtgvG1TtV82kNICeyBJcF.UMECzpICRRUOAK7Dq39ZGZsT6', NULL, NULL, NULL),
-(14, 'Meidy meddy', 'meiddy@yahoo.com', NULL, '$2y$10$ERBWyCbT2H4X4yrHLwoV6.PPW6CX0xK3wguJhHPQImMf10mHRevma', NULL, '2022-01-31 08:20:43', '2022-01-31 08:20:43'),
-(15, 'Dewi Ernst', 'dewiernst@gmail.com', NULL, '$2y$10$9PoGwHKK9OPiQt/qgxkDZ.Z530CMstMPLR3TavQmN/BdslyxH8QX6', NULL, '2022-01-31 08:24:28', '2022-02-01 04:36:18');
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `photo`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Amy Weber', 'hpowlowski@feeney.info', NULL, '$2y$10$9L.Vx2qVpgz4RgPG3lKxruCZFfNhmbARXH5avFsaeUeKIJbZOMyKW', '1643813836.jpg', NULL, NULL, '2022-02-02 07:57:16'),
+(2, 'Otha Cartwright', 'xwindler@hotmail.com', NULL, '$2y$10$ts6N9vIdv7eej5NRCXv1eeUaqPLnIjaxC81NOlrJ5B4tmnOrQ8zNO', '1643813861.jpg', NULL, NULL, '2022-02-02 07:57:41'),
+(3, 'Lowell Durgan', 'npowlowski@stanton.org', NULL, '$2y$10$8DQiHob8mKh/aIEq/JOOp.c5FLXQfNrlKmhcKGxTM3BCKNrLEa6GK', '1643813782.png', NULL, NULL, '2022-02-02 07:56:22'),
+(4, 'Adrian Grant', 'ssanford@yahoo.com', NULL, '$2y$10$e41GYck2VwvcmYe6srEsU.K9pKoipJX2h7BJvElde4WMXKJT1zcp.', '1643813806.png', NULL, NULL, '2022-02-02 07:56:46'),
+(5, 'Linda Gutkowski', 'mertz.tamara@hotmail.com', NULL, '$2y$10$uTMZ8HvtgvG1TtV82kNICeyBJcF.UMECzpICRRUOAK7Dq39ZGZsT6', '1643813796.png', NULL, NULL, '2022-02-02 07:56:36'),
+(14, 'Meidy meddy', 'meiddy@yahoo.com', NULL, '$2y$10$ERBWyCbT2H4X4yrHLwoV6.PPW6CX0xK3wguJhHPQImMf10mHRevma', '1643812928.png', NULL, '2022-01-31 08:20:43', '2022-02-02 07:42:08'),
+(15, 'Dewi Ernst', 'dewiernst@gmail.com', NULL, '$2y$10$9PoGwHKK9OPiQt/qgxkDZ.Z530CMstMPLR3TavQmN/BdslyxH8QX6', '1643813757.png', NULL, '2022-01-31 08:24:28', '2022-02-02 07:55:57');
 
 --
 -- Indexes for dumped tables
@@ -142,7 +144,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
